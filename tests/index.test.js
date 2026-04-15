6a23c512abe694accee695b693e444e6eff67155
Amaan Malik <amikm1077@gmail.com>
2026-04-15 11:01:10 +0530

    Wrote auth test

---
+++ tests/index.test.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+BACKEND_URL = "http://localhost:3000";
+
+describe("Authentication", () => {
+  //checking the signup work
+  test("User's able to sign up only once with same cred: ", async () => {
+    const email = "abeermalik363@gmail.com";
+    const password = "amaaaaaaaaaaaanmalik";
+    const res = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      email,
+      password,
+      type: "admin",
+    });
+
+    expect(res.statusCode).toBe(200);
+
+    const updatedResponse = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      email,
+      password,
+      type: "admin",
+    });
+
+    expect(updatedResponse.statusCode).toBe(400);
+  });
+
+  //checking the signup work
+  test("Test fail if user not pass email: ", async () => {
+    const pass = "amannans";
+    const res = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      pass,
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+
+  //checking the signin work
+  test("Signin succeeed if required field passed:  ", async () => {
+    const email = "something@email.com";
+    const password = "anisoidn";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      email,
+      password,
+    });
+
+    const res = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      email,
+      password,
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+
+  // signin fail work if creds are not correct
+  test("Signin faild if email or password are incorrect:  ", async () => {
+    const email = "something@email.com";
+    const password = "anisoidn";
+
+    await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+      email,
+      password,
+    });
+    const res = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+      email: "wrongemail@gmail.com",
+      password,
+    });
+
+    expect(res.statusCode).toBe(400);
+  });
+});
+
+
+describe('User infoo endpoints', () => {
+  //middleware for auth
+  beforeAll(()=>{})
+
+
+ })
